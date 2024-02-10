8b20dce4741cdc14fd798ca94af0313424af639c
David Omondi Nyalwal <davidomondi842@gmail.com>
2024-02-11 01:28:17 +0300

    first commit

---
+++ backend/sql_connection.py
@@ -0,0 +1,15 @@
+import datetime
+import mysql.connector
+
+__cnx = None
+
+def get_sql_connection():
+  global __cnx
+  if __cnx is None:
+    
+    __cnx = mysql.connector.connect(user='root', password='Omondi@254',
+                                  host='127.0.0.1',
+                                 database='grocerystoredb')
+  return __cnx
+
+
